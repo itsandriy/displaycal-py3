itsandriy/displaycal-py3

commit 9ea4654b22c61bb6659f59c6a0ef5cbcf599d0f8

- Wrapped the pattern strings with ``r""`` in regular expressions. - Replaced ``== None`` and ``!= None`` with ``is None`` and ``is not None``. - Simplified expressions of comparison with two values in ``if`` statements. - Replaced arguments with mutable default values to have ``None`` as their defaults and assigned their default values in the function/method if their values are still ``None``. - Optimized ``DisplayCAL.ICCProfile.ICCProfile.__init__()`` method. - Fixed ``DisplayCAL.ICCProfile.ICCProfile.Unicode`` attribute value logic, which is broken in the previous commit. - Fixed ``binary/str`` issues in ``imfile.py``. - Fixed ``DisplayCAL.network`` by replacing ``getheaders`` calls with ``get``. - Fixed ``DisplayCAL.util_os.mksfile``, which I think is a super unnecessary function. - Fixed ``DisplayCAL.util_str.safe_str`` for ``bytes``. -

DisplayCAL/CGATS.py
@@ -282,7 +282,7 @@ class CGATS(dict):
                 # Replace 1.#IND00 with NaN
                 raw_line = raw_line.replace("1.#IND00", "NaN")
                 # strip control chars and leading/trailing whitespace
-                line = re.sub('[^\x09\x20-\x7E\x80-\xFF]', '',
+                line = re.sub(r'[^\x09\x20-\x7E\x80-\xFF]', '',
                                 raw_line.strip())
                 if '#' in line or '"' in line:
                     # Deal with comments and quotes
@@ -366,7 +366,7 @@ class CGATS(dict):
                     else:
                         if len(values) == 2 and '"' not in values[0]:
                             key, value = values
-                            if value != None:
+                            if value is not None:
                                 context = context.add_data({key: value.strip('"')})
                             else:
                                 context = context.add_data({key: ''})
@@ -376,7 +376,7 @@ class CGATS(dict):
                                                      context.type or "CGATS",
                                                      self.filename or self))
                 elif values and values[0] not in ('Comment:', 'Date:') and \
-                     len(line) >= 3 and not re.search("[^ 0-9A-Za-z/.]", line):
+                     len(line) >= 3 and not re.search(r"[^ 0-9A-Za-z/.]", line):
                     context = self.add_data(line)
             if 0 in self and self[0].get("NORMALIZED_TO_Y_100") == "NO":
                 # Always normalize to Y = 100
@@ -607,7 +607,7 @@ class CGATS(dict):
             while newkey in context['KEYWORDS']:
                 newkey += 1
             context['KEYWORDS'][newkey] = keyword
-        if value != None:
+        if value is not None:
             context[keyword] = value
 
     def add_section(self, key, value):
@@ -790,7 +790,7 @@ class CGATS(dict):
                     if valueslist:
                         values = valueslist.pop(0)
                         checkerboard.append(values)
-        if (shift and checkerboard[-1][:3] == [100, 100, 100]):
+        if shift and checkerboard[-1][:3] == [100, 100, 100]:
             # Move white patch to front
             if debug:
                 print("INFO - moving white to front")
@@ -1614,7 +1614,7 @@ Transform {
                             current_query_value = query[query_key]
                         else:
                             current_query_value = query_value
-                        if current_query_value != None:
+                        if current_query_value is not None:
                             if item[query_key] != current_query_value:
                                 break
                         if get_value:
@@ -1626,7 +1626,7 @@ Transform {
                 if match_count == len(query):
                     if not get_value:
                         result_n = item
-                    if result_n != None:
+                    if result_n is not None:
                         if get_first:
                             if get_value and isinstance(result_n, dict) and \
                                len(result_n) == 1:

commit 6e17719a7f3e5adeb697b19f387ea258b7821f69

- Fixed "Fancy Progress Dialog" not showing up issue, which is introduced in 9ea4654b - Fixed ``CGATS.get_descriptor()`` bug, introduced in 2062ce26

DisplayCAL/CGATS.py
@@ -469,12 +469,12 @@ class CGATS(dict):
                         display = self.queryv1("DISPLAY")
                     if localized:
                         from DisplayCAL import localization as lang
-                        tech = str(tech).encode()
+                        tech = str(tech)
                         tech = lang.getstr("display.tech." + tech, default=tech)
                         if display:
                             # Localized tech will be unicode always, need to
                             # make sure display is as well
-                            display = str(display).encode()
+                            display = str(display)
                     if display:
                         tech += " (%s)" % display
                 desc = tech

DisplayCAL/worker.py
@@ -11851,8 +11851,7 @@ usage: spotread [-options] [logfile]
 
     def progress_handler(self, event):
         """ Handle progress dialog updates and react to Argyll CMS command output """
-        if getattr(self, "subprocess_abort", False) or \
-                getattr(self, "thread_abort", False):
+        if getattr(self, "subprocess_abort", False) or getattr(self, "thread_abort", False):
             self.progress_wnd.Pulse(lang.getstr("aborting"))
             return
         percentage = None
@@ -11870,7 +11869,7 @@ usage: spotread [-options] [logfile]
                 percentage = int(self.lastmsg.read().split("%")[0])
             except ValueError:
                 pass
-        elif re.match(r"Patch \\d+ of \\d+", lastmsg, re.I):
+        elif re.match(r"Patch \d+ of \d+", lastmsg, re.I):
             # dispcal/dispread
             components = lastmsg.split()
             try:
@@ -11880,7 +11879,7 @@ usage: spotread [-options] [logfile]
                 pass
             else:
                 percentage = max(start - 1, 0) / end * 100
-        elif re.match(r"Added \\d+/\\d+", lastmsg, re.I):
+        elif re.match(r"Added \d+/\d+", lastmsg, re.I):
             # targen
             components = lastmsg.lower().replace("added ", "").split("/")
             try:
@@ -11891,7 +11890,7 @@ usage: spotread [-options] [logfile]
             else:
                 percentage = start / end * 100
         else:
-            iteration = re.search(r"It (\\d+):", msg)
+            iteration = re.search(r"It (\d+):", msg)
             if iteration:
                 # targen
                 try:
@@ -12804,10 +12803,10 @@ usage: spotread [-options] [logfile]
                 # 'path/to/1.gam' volume = xxx.x cubic units, intersect = xx.xx%
                 # 'path/to/2.gam' volume = xxx.x cubic units, intersect = xx.xx%
                 for line in worker.output:
-                    match = re.search(r"[\\\/]%s.gam'\s+volume\s*=\s*"
-                                      "\d+(?:\.\d+)?\s+cubic\s+units,?"
-                                      "\s+intersect\s*=\s*"
-                                      "(\d+(?:\.\d+)?)" %
+                    match = re.search(r"[\/]%s.gam'\s+volume\s*=\s*"
+                                      r"\d+(?:\.\d+)?\s+cubic\s+units,?"
+                                      r"\s+intersect\s*=\s*"
+                                      r"(\d+(?:\.\d+)?)" %
                                       re.escape(src), line)
                     if match:
                         gamut_coverage[key] = float(match.groups()[0]) / 100.0
@@ -14569,7 +14568,7 @@ BEGIN_DATA
                 self.log("%s: Patch update count: %i" %
                          (appname, self.patch_count))
         if self.use_madnet_tpg:
-            progress = re.search(r"(?:Patch (\\d+) of|Number of patches =) (\\d+)", txt, re.I)
+            progress = re.search(r"(?:Patch (\d+) of|Number of patches =) (\d+)", txt, re.I)
             if progress:
                 # Set madTPG progress bar
                 try: