d5f77b48bcd568a3faac3a6b4de9d02551b38531
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2022-03-13 16:39:38 +0000

    - Matched ``DisplayCAL.wxwindows.fancytext_RenderToRenderer()`` to PR https://github.com/eoyilmaz/displaycal-py3/pull/33.

--- DisplayCAL/wxwindows.py
+++ DisplayCAL/wxwindows.py
@@ -5247,7 +5247,7 @@ def fancytext_RenderToRenderer(text, renderer, enclose=True):
         p.CharacterDataHandler = renderer.characterData
         p.Parse(text, True)
     except xml.parsers.expat.error as err:
-        raise ValueError('error parsing text text "{text}": {err}')
+        raise ValueError(f'error parsing text text "{text}": {err}')
 
 
 fancytext.RenderToRenderer = fancytext_RenderToRenderer
