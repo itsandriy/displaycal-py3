656b37eb3ad0ea29cccbb84f169ad5a18f79bc47
patrick <patrick@p5k.org>
2022-03-16 21:16:18 +0100

    commented additional test which uses initcfg

--- tests/test_wx_measure_frame.py
+++ tests/test_wx_measure_frame.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
-
-def test_get_default_size_1():
-    """Testing wxMeasureFrame.get_default_size() function."""
-    from DisplayCAL.config import initcfg
-    from DisplayCAL.wxMeasureFrame import get_default_size
-    initcfg()
-    import wx
-    app = wx.GetApp()
-    if not app:
-        app = wx.PySimpleApp()
-    result = get_default_size()
-    assert isinstance(result, int)
-    assert result > 1
+#from DisplayCAL import config
+#from DisplayCAL import RealDisplaySizeMM
+# def test_get_default_size_1():
+#     """Testing wxMeasureFrame.get_default_size() function."""
+#     from DisplayCAL.config import initcfg
+#     from DisplayCAL.wxMeasureFrame import get_default_size
+#     initcfg()
+#     import wx
+#     app = wx.GetApp()
+#     if not app:
+#         app = wx.PySimpleApp()
+#     result = get_default_size()
+#     assert isinstance(result, int)
+#     assert result > 1
