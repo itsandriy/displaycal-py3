f312b4050f832d33e34b567f9e591534fbf13ad2
patrick <patrick@p5k.org>
2022-03-19 00:29:33 +0100

    removed app creation due to total mock of wx

--- tests/test_wx_measure_frame.py
+++ tests/test_wx_measure_frame.py
@@ -2,30 +2,25 @@
 
 import wx
 
-from DisplayCAL import RealDisplaySizeMM, config
+from DisplayCAL import RealDisplaySizeMM
 from DisplayCAL.dev.mocks import check_call, check_call_str
 from DisplayCAL.wxMeasureFrame import get_default_size
 from tests.data.display_data import DisplayData
 
-# todo: deactivated test temporary because of pipeline issue
-#  INTERNALERROR>   File "/opt/hostedtoolcache/Python/3.8.12/x64/lib/python3.8/posixpath.py", line 379, in abspath
-#  NTERNALERROR>     cwd = os.getcwd()
-#  INTERNALERROR> FileNotFoundError: [Errno 2] No such file or directorys
 
-
-def test_get_default_size_1():
+def test_get_default_size_1() -> None:
     """Testing wxMeasureFrame.get_default_size() function."""
-    app = wx.GetApp()
-    if not app:
-        app = wx.PySimpleApp()
     with check_call_str("DisplayCAL.wxMeasureFrame.getcfg", DisplayData.CFG_DATA):
-        with check_call_str("DisplayCAL.wxMeasureFrame.get_display_number",
-                        DisplayData.DISPLAY_DATA["screen"]):
-            with check_call(wx, "Display", DisplayData(), call_count=2):
-            #with check_call(wx, "DisplaySizeMM", DisplayData.DISPLAY_DATA["size_mm"]):
-                with check_call(RealDisplaySizeMM, "RealDisplaySizeMM", DisplayData.DISPLAY_DATA["size"]):
-            #with check_call(wx, "DisplaySize", DisplayData.DISPLAY_DATA["size"]):
-                #with check_call(wx, "DisplaySizeMM", DisplayData.DISPLAY_DATA["size_mm"]):
+        with check_call_str(
+            "DisplayCAL.wxMeasureFrame.get_display_number",
+            DisplayData.DISPLAY_DATA["screen"],
+        ):
+            with check_call(wx, "Display", DisplayData()):
+                with check_call(
+                    RealDisplaySizeMM,
+                    "RealDisplaySizeMM",
+                    DisplayData.DISPLAY_DATA["size"],
+                ):
                     result = get_default_size()
     assert isinstance(result, int)
     assert result > 1
