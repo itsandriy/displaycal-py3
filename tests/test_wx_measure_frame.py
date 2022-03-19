8628c6715040dc24b9c4a52718a03955c4a551ee
patrick <patrick@p5k.org>
2022-03-19 01:37:49 +0100

    testcase test_get_default_size_1 refactored

--- tests/test_wx_measure_frame.py
+++ tests/test_wx_measure_frame.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from typing import Tuple
 
+import pytest
 import wx
+from _pytest.fixtures import SubRequest
 
 from DisplayCAL import RealDisplaySizeMM
 from DisplayCAL.dev.mocks import check_call, check_call_str
@@ -8,7 +11,24 @@ from DisplayCAL.wxMeasureFrame import get_default_size
 from tests.data.display_data import DisplayData
 
 
-def test_get_default_size_1() -> None:
+@pytest.fixture(
+    scope="session", name="size_in_mm", params=["size_available", "size_unavailable"]
+)
+def fixture_size_in_mm(request: SubRequest) -> Tuple[int, int]:
+    """Return display size in mm (width, height)."""
+    return (
+        DisplayData.DISPLAY_DATA["size"]
+        if request.param == "size_available"
+        else (0, 0)
+    )
+
+
+@pytest.mark.parametrize(
+    "real_display",
+    (True, False),
+    ids=("with_RealDisplaySizeMM", "without_RealDisplaySizeMM"),
+)
+def test_get_default_size_1(real_display: bool, size_in_mm: Tuple[int, int]) -> None:
     """Testing wxMeasureFrame.get_default_size() function."""
     with check_call_str("DisplayCAL.wxMeasureFrame.getcfg", DisplayData.CFG_DATA):
         with check_call_str(
@@ -16,11 +36,18 @@ def test_get_default_size_1() -> None:
             DisplayData.DISPLAY_DATA["screen"],
         ):
             with check_call(wx, "Display", DisplayData()):
-                with check_call(
-                    RealDisplaySizeMM,
-                    "RealDisplaySizeMM",
-                    DisplayData.DISPLAY_DATA["size"],
-                ):
-                    result = get_default_size()
+                if real_display:
+                    with check_call(
+                        RealDisplaySizeMM,
+                        "RealDisplaySizeMM",
+                        DisplayData.DISPLAY_DATA["size"],
+                    ):
+                        result = get_default_size()
+                else:
+                    with check_call(
+                        wx, "DisplaySize", DisplayData.DISPLAY_DATA["size"]
+                    ):
+                        with check_call(wx, "DisplaySizeMM", size_in_mm):
+                            result = get_default_size()
     assert isinstance(result, int)
     assert result > 1
